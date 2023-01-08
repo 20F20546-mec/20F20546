34e970d1065ced7dec3c84ff356a0a8141e80827
20F20546-mec <118101059+20F20546-mec@users.noreply.github.com>
2023-01-08 21:19:38 +0400

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+  public static void main(String args[]) {
+    Scanner sc = System.in;
+    System.out.println("Enter the first number: ");
+    int a = sc.nextInt();
+    System.out.println("Enter the second number: ");
+    int b = sc.nextInt();
+    int Sum = a - b;
+    System.out.println("Sum: " + Sum);
+    }
+   }
+  }
